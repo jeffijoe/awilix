028f76585f09789c432c74d14422531eab253065
Jeff Hansen <jeffijoe@hotmail.com>
2016-02-24 20:48:11 +0100

    loadModules function implemented and tested. 100% coverage.

--- test/lib/loadModules.spec.js
+++ test/lib/loadModules.spec.js
@@ -2,30 +2,38 @@
 const loadModules = require('../../lib/loadModules');
 const spy = sinon.spy;
 
-describe('loadModules', function() {
-  // it('calls the default export with the container', function() {
-  //   const modules = {
-  //     ['m1.js']: spy(),
-  //     ['m2.js']: spy()
-  //   };
-  //   const container = {
+const createTimeout = () => new Promise(resolve => setTimeout(resolve, 1));
 
-  //   };
-  //   const deps = {
-  //     container,
-  //     listModules: spy(
-  //       () => Promise.resolve(
-  //         [
-  //           { name: 'm1', path: 'm1.js'},
-  //           { name: 'm2', path: 'm2.js'}
-  //         ]
-  //       )
-  //     ),
-  //     require: spy(path => modules[path])
-  //   };
-  //   const opts = {};
-  //   return loadModules(deps, 'anything', opts).then(result => {
-  //     result.should.equal(container);
-  //   });
-  // });
+describe('loadModules', function() {
+  it('calls the default export with the container', function() {
+    const modules = {
+      ['standard.js']: spy(),
+      ['promise.js']: spy(() => createTimeout()),
+      ['default.js']: { default: spy() },
+      ['default-promise.js']: { default: spy(() => createTimeout()) }
+    };
+    const container = {
+
+    };
+    const deps = {
+      container,
+      listModules: spy(
+        () => Promise.resolve(
+          Object.keys(modules).map(key => ({
+            name: key.replace('.js', ''),
+            path: key
+          }))
+        )
+      ),
+      require: spy(path => modules[path])
+    };
+    const opts = {};
+    return loadModules(deps, 'anything', opts).then(result => {
+      result.should.equal(container);
+      Object.keys(modules).map(x => modules[x]).forEach(m => {
+        if (m.default) m = m.default;
+        m.should.have.been.calledWith(container);
+      });
+    });
+  });
 });
