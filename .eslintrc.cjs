6a3168ebe4257c589942bb3140e2e83ef2cad410
Jeff Hansen <jeffijoe@hotmail.com>
2023-10-12 09:18:13 -0400

    style: add eslint-config-prettier to turn off formatting lints

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,10 @@
 /* eslint-env node */
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
