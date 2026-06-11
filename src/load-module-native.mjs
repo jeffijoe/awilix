d35803dd3fdc742c32b57b161f2aabd28a03143a
Jeff Hansen <jeffijoe@hotmail.com>
2026-06-11 06:33:16 -0400

    chore(build): greatly simplify Rollup build

--- src/load-module-native.mjs
+++
@@ -1,4 +0,0 @@
-// This is kept in a separate .js file to prevent TypeScript re-writing the import() statement to a require() statement
-export function importModule(path) {
-  return import(path)
-}
